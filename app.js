c97c6fe6a3bc1a65186872c79a4bc6ae6297e434
francois-did <francoisdidouche@gmail.com>
2025-05-17 10:22:22 +0200

    deuxieme commit

--- app.js
+++ app.js
@@ -25,30 +25,41 @@ intro.textContent = "Salut tout le monde"
 
 const projets = [
   {
-    "nom": "Booky",
-    "image": "images/portrait.jpg",
-    "description": "site vitrine en html css",
-    "technologies": "html/css"
+    nom: "Booky",
+    image: "images/portrait.jpg",
+    description: "site vitrine en html css",
+    technologies: "html/css"
   },
-
   {
-    "nom": "Sophie Bluel",
-    "image": "images/portrait.jpg",
-    "description": "développer pour une architecte d'intérieur ",
-    "technologies": "Javascript"
+    nom: "Sophie Bluel",
+    image: "images/portrait.jpg",
+    description: "développer pour une architecte d'intérieur",
+    technologies: "Javascript"
   },
-
   {
-    "nom": "Kasa",
-    "image": "images/portrait.jpg",
-    "description": "ront-end avec React et React Router Dom",
-    "technologies": "React, SCSS"
+    nom: "Kasa",
+    image: "images/portrait.jpg",
+    description: "front-end avec React et React Router Dom",
+    technologies: "React, SCSS"
   },
+  {
+    nom: "Booky (copy)",
+    image: "images/portrait.jpg",
+    description: "site vitrine en html css (copie)",
+    technologies: "html/css"
+  },
+  {
+    nom: "Kasa (copy)",
+    image: "images/portrait.jpg",
+    description: "version doublée de Kasa",
+    technologies: "React"
+  }
 ];
 
-const projetsContainer = document.getElementById("projets");
+const projetsContainer = document.querySelector(".projets-carousel");
 
-    projets.forEach((projet) => {
+
+projets.forEach((projet) => {
   const card = `
     <div class="carte-projet">
       <img src="${projet.image}" alt="${projet.nom}">
@@ -57,11 +68,48 @@ const projetsContainer = document.getElementById("projets");
         <p class="projet-description">${projet.description}</p>
         <p class="projet-techno">${projet.technologies}</p>
       </div>
-      
     </div>
   `;
   projetsContainer.innerHTML += card;
 });
+
+// Carousel JS
+let scrollAmount = 0;
+const scrollStep = projetsContainer.querySelector(".carte-projet").offsetWidth + 24;
+
+
+document.querySelector(".carousel-btn.left").addEventListener("click", () => {
+  if (projetsContainer.scrollLeft <= 0) {
+    projetsContainer.scrollTo({
+      left: projetsContainer.scrollWidth,
+      behavior: "smooth"
+    });
+  } else {
+    projetsContainer.scrollBy({
+      left: -scrollStep,
+      behavior: "smooth"
+    });
+  }
+});
+
+
+document.querySelector(".carousel-btn.right").addEventListener("click", () => {
+  const maxScroll = projetsContainer.scrollWidth - projetsContainer.clientWidth;
+
+  if (projetsContainer.scrollLeft >= maxScroll) {
+    projetsContainer.scrollTo({
+      left: 0,
+      behavior: "smooth"
+    });
+  } else {
+    projetsContainer.scrollBy({
+      left: scrollStep,
+      behavior: "smooth"
+    });
+  }
+});
+
+
 
 
 
